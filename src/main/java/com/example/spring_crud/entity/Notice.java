718efeab976181125589ea9854974b25bd77a049
CordHouse <qkqktl5310@gmail.com>
2022-09-08 18:19:28 +0900

    feat : Add title, content, writer, time

--- src/main/java/com/example/spring_crud/entity/Notice.java
+++ src/main/java/com/example/spring_crud/entity/Notice.java
@@ -3,11 +3,10 @@ package com.example.spring_crud.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +16,27 @@ public class Notice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private LocalDate date;
+
+    public Notice(String title, String content, String writer){
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+
+    @PrePersist
+    public void createDate(){
+        this.date = LocalDate.now();
+    }
 }
