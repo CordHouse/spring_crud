a208d0a8238210d9963706cdcdd4db46a34be259
CordHouse <qkqktl5310@gmail.com>
2022-09-08 18:20:56 +0900

    Feat : Add CreateDto

--- src/main/java/com/example/spring_crud/dto/NoticeCreateRequestDto.java
+++ src/main/java/com/example/spring_crud/dto/NoticeCreateRequestDto.java
@@ -1,4 +1,19 @@
 package com.example.spring_crud.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NoticeCreateRequestDto {
+    @NotBlank(message = "게시글 제목을 입력해주세요.")
+    private String title;
+    @NotBlank(message = "게시글 내용을 입력해주세요.")
+    private String content;
+    @NotBlank(message = "게시글 작성자를 입력해주세요.")
+    private String writer;
 }
