2bbd6807555e624ba4a9cd897cfab658146d1d5d
CordHouse <qkqktl5310@gmail.com>
2022-10-03 22:06:08 +0900

    Refactor : NoticeService -> Response -> void

--- src/main/java/com/example/spring_crud/service/notice/NoticeService.java
+++ src/main/java/com/example/spring_crud/service/notice/NoticeService.java
@@ -31,26 +31,24 @@ public class NoticeService {
 
     // 게시글 검색 조회
     @Transactional
-    public List<SearchNoticesResponseDto> searchNoticesTitle(SearchNoticesTitleRequestDto searchNoticesTitleRequestDto){
+    public void searchNoticesTitle(SearchNoticesTitleRequestDto searchNoticesTitleRequestDto){
         List<Notice> notices = noticeRepository.findAllByTitleContaining(searchNoticesTitleRequestDto.getTitle()).orElseThrow();
         if(notices.isEmpty())
             throw new NoticeBoardTitleNotFoundException();
         List<SearchNoticesResponseDto> searchNoticesResponseDtoList = new LinkedList<>();
         notices.forEach(noticeTitle -> searchNoticesResponseDtoList.add(new SearchNoticesResponseDto().toDto(noticeTitle)));
-        return searchNoticesResponseDtoList;
     }
 
     // 게시글 생성
     @Transactional
-    public NoticeCreateResponseDto createBoards(NoticeCreateRequestDto noticeCreateRequestDto){
+    public void createBoards(NoticeCreateRequestDto noticeCreateRequestDto){
         Notice notices = new Notice(noticeCreateRequestDto.getTitle(), noticeCreateRequestDto.getContent(), noticeCreateRequestDto.getWriter());
         noticeRepository.save(notices);
-        return new NoticeCreateResponseDto().toDto(notices);
     }
 
     // 게시글 수정 ( title, content )
     @Transactional
-    public NoticeEditResponseDto editBoardsContent(Long id, NoticeEditRequestDto noticeEditRequestDto){
+    public void editBoardsContent(Long id, NoticeEditRequestDto noticeEditRequestDto){
         Notice notice = noticeRepository.findById(id).orElseThrow(() -> {
             throw new NoticeBoardsNotFoundException();
         });
@@ -62,45 +60,40 @@ public class NoticeService {
         if(!noticeEditRequestDto.getContent().equals("")) {
             notice.setContent(noticeEditRequestDto.getContent());
         }
-        return new NoticeEditResponseDto().toDto(notice);
     }
 
     // 작성자 조회
     @Transactional
-    public List<SearchNoticesResponseDto> searchNoticesWriter(SearchNoticesWriterRequestDto searchNoticesWriterRequestDto){
+    public void searchNoticesWriter(SearchNoticesWriterRequestDto searchNoticesWriterRequestDto){
         List<Notice> notices = noticeRepository.findAllByWriterContaining(searchNoticesWriterRequestDto.getWriter()).orElseThrow();
         if(notices.isEmpty())
             throw new NoticeBoardTitleNotFoundException();
         List<SearchNoticesResponseDto> searchNoticesResponseDtoList = new LinkedList<>();
         notices.forEach(noticeWriter -> searchNoticesResponseDtoList.add(new SearchNoticesResponseDto().toDto(noticeWriter)));
-        return searchNoticesResponseDtoList;
     }
 
     // 날짜별 조회
     @Transactional
-    public List<SearchNoticesDateResponseDto> searchNoticesDate(SearchNoticesDateRequestDto searchNoticesDateRequestDto){
+    public void searchNoticesDate(SearchNoticesDateRequestDto searchNoticesDateRequestDto){
         List<Notice> notice = noticeRepository.findAllByDateBetween(searchNoticesDateRequestDto.getStartDate(), searchNoticesDateRequestDto.getEndDate()).orElseThrow();
         if(notice.isEmpty()){
             throw new NoticeNotFoundSearchDateException();
         }
         List<SearchNoticesDateResponseDto> searchNoticesDateResponseDtoList = new LinkedList<>();
         notice.forEach(date -> searchNoticesDateResponseDtoList.add(new SearchNoticesDateResponseDto().toDto(date)));
-        return searchNoticesDateResponseDtoList;
     }
 
     // 게시글 삭제
     @Transactional
-    public String deleteBoards(Long id){
+    public void deleteBoards(Long id){
         Notice notice = noticeRepository.findById(id).orElseThrow(() -> {
             throw new NoticeBoardsNotFoundException();
         });
         noticeRepository.deleteById(notice.getId());
-        return id + "번 게시글이 삭제되었습니다.";
     }
     // 게시글 전체 삭제
     @Transactional
-    public String deleteAllBoards(){
+    public void deleteAllBoards(){
         noticeRepository.deleteAll();
-        return "게시글이 전부 삭제되었습니다.";
     }
 }
